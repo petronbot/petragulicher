a7636606085353a5dadc1396bd0b619784280a84
petronbot <petrag@themonkeys.com.au>
2015-03-20 16:12:09 +1100

    Errthing for v3.0

--- build/web/js/main.js
+++ build/web/js/main.js
@@ -2,12 +2,43 @@
     'use strict';
 
     require.config({
+        shim: {
+            'tooltipster': {
+                deps: ['jquery'],
+                exports: 'tooltipster'
+            },
+            'singlePageNav': {
+                deps: ['jquery'],
+                exports: 'singlePageNav'
+            }
+        },
         paths: {
-            "jquery": "../bower_components/jquery/dist/jquery"
+            "jquery": "../bower_components/jquery/dist/jquery",
+            "tooltipster": "../bower_components/tooltipster/js/jquery.tooltipster",
+            "singlePageNav": "vendor/plugins/jquery.singlePageNav",
+            "skrollr": "../bower_components/skrollr/dist/skrollr.min"
         }
     });
-    require(['jquery'], function ($) {
-        console.log($);
+    require(['jquery', 'skrollr', 'tooltipster', 'singlePageNav'], function ($, skrollr) {
+        $(window).ready(function () {
+
+            // Init tooltips
+            $('.tooltip').tooltipster({
+                theme: 'tooltipster-light'
+            });
+
+            // Init scrolling nav
+            $('.section-links').singlePageNav();
+
+            // Init scroll effects
+            $('.work-tile').attr({
+                'data-start': 'margin-top:3em',
+                'data--100-bottom': 'margin-top:0em'
+            });
+            this.scrolling = skrollr.init({
+                forceHeight: false
+            });
+
+        });
     });
 })();
-
